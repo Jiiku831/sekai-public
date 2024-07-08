fa5e948fc56626a6f40ac3674707676dc36fe843
Jiiku831 <142133559+Jiiku831@users.noreply.github.com>
2024-07-08 02:26:09 -0700

    move registry to separate repo

--- test.sh
+++ test.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/bash
+if [[ "$(git rev-parse --abbrev-ref HEAD)" = "en" ]]; then
+  exit 0
+fi
 BAZEL_CXXOPTS=-std=c++20 bazelisk test -c opt \
     --build_tests_only --test_tag_filters=-manual --test_output=summary ...
