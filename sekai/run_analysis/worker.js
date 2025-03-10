539ebf5e69ccec7eefa17d839057d687c0dc627e
Jiiku831 <142133559+Jiiku831@users.noreply.github.com>
2025-03-10 04:01:18 -0700

    move wasm initialization to startup

--- sekai/run_analysis/worker.js
+++ sekai/run_analysis/worker.js
@@ -1,15 +1,27 @@
 import Module from "./server_main.js";
 
-function stringToNewUTF8(module, str) {
+var module = null;
+async function initModule() {
+  module = await Module({
+    instantiateWasm: (imports, callback) => {
+     const instance = new WebAssembly.Instance(WASM_MAIN, imports);
+      callback(instance);
+      return instance.exports;
+    },
+  });
+}
+initModule().then(() => { console.log("WASM Module Initialized"); });
+
+function stringToNewUTF8(str) {
   let bufSize = module.lengthBytesUTF8(str) + 1;
   let ptr = module._Malloc(bufSize);
   module.stringToUTF8(str, ptr, bufSize);
   return ptr;
 }
 
-function callHandler(module, path, input) {
-  const inputPtr = stringToNewUTF8(module, input);
-  const pathPtr = stringToNewUTF8(module, path);
+function callHandler(path, input) {
+  const inputPtr = stringToNewUTF8(input);
+  const pathPtr = stringToNewUTF8(path);
   const resultPtr = module._HandleRequest(pathPtr, inputPtr);
   const code = module.getValue(resultPtr, "i8");
   const result = module.UTF8ToString(resultPtr + 1);
@@ -19,58 +31,25 @@ function callHandler(module, path, input) {
   return [code, result];
 }
 
-var m = null;
-
-async function getModule() {
-  if (m != null) {
-    return m;
-  }
-  m = await Module({
-    instantiateWasm: (imports, callback) => {
-     const instance = new WebAssembly.Instance(WASM_MAIN, imports);
-      callback(instance);
-      return instance.exports;
-    },
-  });
-  return m;
-}
-
 function mapCode(code) {
   return {
-    // OK
-    0: 200,
-    // Cancelled
-    1: 500,
-    // Unknown
-    2: 500,
-    // Invalid Argument
-    3: 400,
-    // Deadline Exceeded
-    4: 500,
-    // Not Found
-    5: 404,
-    // Already Exists
-    6: 500,
-    // Permission Denied
-    7: 403,
-    // Resource Exhausted
-    8: 500,
-    // Failed Precondition
-    9: 500,
-    // Aborted
-    10: 500,
-    // Out of Range
-    11: 500,
-    // Unimplemented
-    12: 501,
-    // Internal
-    13: 500,
-    // Unavailable
-    14: 503,
-    // Data Loss
-    15: 500,
-    // Unauthenticated
-    16: 401,
+    0: 200,   // OK
+    1: 500,   // Cancelled
+    2: 500,   // Unknown
+    3: 400,   // Invalid Argument
+    4: 500,   // Deadline Exceeded
+    5: 404,   // Not Found
+    6: 500,   // Already Exists
+    7: 403,   // Permission Denied
+    8: 500,   // Resource Exhausted
+    9: 500,   // Failed Precondition
+    10: 500,  // Aborted
+    11: 500,  // Out of Range
+    12: 501,  // Unimplemented
+    13: 500,  // Internal
+    14: 503,  // Unavailable
+    15: 500,  // Data Loss
+    16: 401,  // Unauthenticated
   }[code] || 500;
 }
 
@@ -78,9 +57,8 @@ async function handleRequest(request) {
   let output = "";
 
   const url = new URL(request.url);
-  let module = getModule();
   let body = request.text();
-  let [code, result] = callHandler(await module, url.pathname, await body);
+  let [code, result] = callHandler(url.pathname, await body);
   return new Response(result, {
     status: mapCode(code),
   });
